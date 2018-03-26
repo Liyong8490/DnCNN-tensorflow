e26c7eea0986ba6ae28bb0b50f87428087df5404
DESKTOP-1O16DA4\Lee <liyong_xgd@outlook.com>
2018-03-26 18:43:05 +0800

    no message

---
+++ model_DnCNN.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+class DnCNN(object):
+	"""docstring for DnCNN"""
+	def __init__(self, input_chnl):
+		super(DnCNN, self).__init__()
+		self.input_chnl = input_chnl
+		return 0
+	
+    def build_model()
+    	return 0
+
+	def train()
+
+		return 0
